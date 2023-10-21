8ed40f74944f468ecb4c59460c5f761e0b138bb6
BuildTools <unconfigured@null.spigotmc.org>
2023-10-21 13:18:00 -0400

    ADDED GARBAGE TO ADD MY OWN COLORS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
 <script>
 	import {Card, CardBody, CardFooter, CardHeader, CardTitle} from 'sveltestrap';
 	import Logo from "../lib/assets/LightningbulbLogo.png"
+
+	import { slide } from 'svelte/transition';
+	let showingCard = true;
+
+	function toggleCard() {
+		showingCard = !showingCard;
+	}
+
 </script>
 
 <svelte:head>
@@ -9,18 +17,50 @@
 </svelte:head>
 
 <div class="d-flex justify-content-center p-5">
-	<h1 class="test">HIHIHI</h1>
 
-	<Card class="w-25 shadow-lg bg-myown">
-		<CardHeader>
-			<CardTitle class="text-center text-light">Welcome to Digital Hobbies</CardTitle>
-		</CardHeader>
-		<CardBody>
-			<img src={Logo} class="w-100 rounded-2" alt="Lightningbulb Logo">
-		</CardBody>
-		<CardFooter class="text-center text-light">
-			A Lightningbulb website.
-		</CardFooter>
+
+	<Card style="width: 20rem" class="shadow-lg bg-notquiteblack text-center text-light">
+		<div style="height: 25rem" class=" shadow-lg bg-notequiteblack"
+			 on:mouseenter={() => {toggleCard();}}
+			 on:mouseleave={() => {toggleCard();}}>
+
+		{#if showingCard}
+
+			<div transition:slide|local>
+
+				<CardHeader>
+					<CardTitle>Welcome to Digital Hobbies</CardTitle>
+				</CardHeader>
+				<CardBody>
+					<img src={Logo} class="w-100 rounded-2" alt="Lightningbulb Logo">
+				</CardBody>
+				<CardFooter>
+					A Lightningbulb website.
+				</CardFooter>
+
+			</div>
+
+		{/if}
+
+		{#if !showingCard}
+
+			<div transition:slide|local>
+
+				<CardHeader>
+					<CardTitle>Lightningbulb.png</CardTitle>
+				</CardHeader>
+				<CardBody>
+					I made this image in photoshop,<br> March of 2020.
+				</CardBody>
+
+			</div>
+
+		{/if}
+
+		</div>
 	</Card>
 
+
 </div>
+
+
